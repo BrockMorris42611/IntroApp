f168310148fa9ff40e6683230a12e210dd1ee5ef
BrockMorris42611 <brock36morris@gmail.com>
2021-02-04 19:48:20 -0500

    I set up a new button that changes the value of TextView variable text_box. I did this by attaching a listener to the button(found via seraching for the id of the button i made) that waits for clicks and then replaces the text in the textbox with a new string that is residing in the strings.xml file.

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -1,8 +1,13 @@
+//Brock Morris tuj42611
+//Karl Morris, Intro to App Dev
+//2-4-21
 package edu.temple.helloworld;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button text_change_button = findViewById(R.id.button); //we have a button with id: button that we need to find so we search by id
+
+        text_change_button.setOnClickListener(new View.OnClickListener() { //now we set up a listener that waits for clicks
+            @Override
+            public void onClick(View v) { //when the listener is working and detects a press on the button then it moves to change the text
+                TextView textBox = findViewById(R.id.text_box); //again i am searching for a TextView sincce we are looking for a text box. identify it by its id: text_box
+                textBox.setText(R.string.new_text_mssg); //set that text in the box to the last string shown in the strings.xml
+            }
+        });
     }
 
 }
